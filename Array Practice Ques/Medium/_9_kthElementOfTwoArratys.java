8bfe0c7b297de7ad24283e7b24deb4243c2b37ac
VikasKare <karevikas7777@gmail.com>
2024-01-26 23:52:09 +0530

    Array Practice Questions PDF's

---
+++ Array Practice Ques/Medium/_9_kthElementOfTwoArratys.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+
+class _9_kthElementOfTwoArratys {
+
+    static int main;
+    public static void main(String[] args) {
+
+        main = 1;
+        System.out.println(main++);
+        
+        main(args);
+    }
+}
