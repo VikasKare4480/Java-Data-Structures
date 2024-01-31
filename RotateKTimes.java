e62bebfffdc075f95ef48c77d3e79efbe4d751c9
VikasKare <karevikas7777@gmail.com>
2024-01-31 22:47:56 +0530

    RotateArrayKTimes

---
+++ RotateKTimes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class RotateKTimes {
+
+
+    static void rotateKTimes(int[] arr, int k) {
+
+        
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the K : ");
+        int k = sc.nextInt();
+    }
+}
