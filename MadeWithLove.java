3a2aebbf22ba7ffd598f83513fd0a07f322fcfd9
VikasKare <karevikas7777@gmail.com>
2024-02-04 00:05:16 +0530

    Made-with-Love.com

---
+++ MadeWithLove.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+import javafx.scene.transform.Scale;
+class MadeWithLove  {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter Last Name : ");
+        String lastName = sc.nextLine();
+        
+
+        System.out.println("Wel-Come " + firstName + " " + lastName);
+
+
+        sc.close();
+    }
+}
