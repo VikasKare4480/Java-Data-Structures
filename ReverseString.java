d5fb6f04fea94948072a2c54ba8a2cb02fa90a55
Vikas Kare <91896131+VikasKare4480@users.noreply.github.com>
2024-02-10 18:55:09 +0000

    Palindrome Feature Added

--- ReverseString.java
+++ ReverseString.java
@@ -9,7 +9,15 @@ class ReverseString {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter the String to reverse : ");
         String str = br.readLine();
-        System.out.println("This is the reversethe " + reverseString(str));
+        System.out.println("This is the reverse :  " + reverseString(str));
+
+        if(isPalindrome(str)) {
+
+            System.out.println(str + " is palindrome String");
+        }else {
+
+            System.out.println(str + " is not palindrome String");
+        }
     }
 
     static String reverseString(String str) {
@@ -18,4 +26,12 @@ class ReverseString {
         return sb.reverse().toString();
     }
 
+    static boolean isPalindrome(String str) {
+
+        System.out.println(str.getClass());
+        System.out.println(reverseString(str).getClass());
+        System.out.println(System.identityHashCode(str));
+        System.out.println(System.identityHashCode(reverseString(str)));
+        return str == reverseString(str);
+    }
 }
