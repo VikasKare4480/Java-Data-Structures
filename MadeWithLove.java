bc78ad081ee18f62660b05442b4f029aca24d9ba
VikasKare <karevikas7777@gmail.com>
2024-03-11 21:45:03 +0530

    File added

--- MadeWithLove.java
+++ MadeWithLove.java
@@ -6,18 +6,12 @@ class MadeWithLove  {
 
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Name : ");
         String firstName = sc.nextLine();
-
         System.out.println("Enter Last Name : ");
         String lastName = sc.nextLine();
-        
-
         System.out.println("Wel-Come " + firstName + " " + lastName);
-
-
         sc.close();
     }
 }
